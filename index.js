87c216e24b1082b434304b46e21ff3e0caa38d0b
aplucche <aplucche@gmail.com>
2016-06-16 12:03:22 -0400

    Connect Graphic to Lambda Function API

--- index.js
+++ index.js
@@ -10,44 +10,67 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-        data : [
-          ['a',12.5],
-          ['b',10.7],
-          ['c',9.7],
-          ['d',8.5],
-          ['e',7.7],
-          ['f',7.6],
-          ['g',7.5],
-          ['h',7.4],
-          ['i',7.3],
-          ['j',7.2],
-          ['k',7.1],
-          ['l',1.5]
-        ]
-      };
+        data : [['',0]]
+    };
+  }
+  fetchRSSFeed(url) {
+    var body = {'url':url}
+    fetch('https://gmr99cgdc5.execute-api.us-east-1.amazonaws.com/v1/rss', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {  
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body)
+      }).then(r => r.json())
+  .then(data => {
+    this.setState({data:JSON.parse(data)})
+    })
+  }
+  componentDidMount() {
+    this.fetchRSSFeed('http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml');
+  }
+  handleDropdownChange(e) {
+    this.fetchRSSFeed(e.target.value);
   }
   render () {
-    return (<div class='appContainer'>
-    <h1>Minimal Setup with Webpack and SASS</h1>
-    <Dropdown/>
+    return (<div className='appContainer'>
+    <h1>Frequency of Words in the News</h1>
+    <p>Word count of headlines and article descriptions from RSS feeds of popular publications</p>
+    <Dropdown handleDropdownChange={this.handleDropdownChange.bind(this)}/>
     <Graphic data={this.state.data}/>
     </div>);
   }
 }
 
 class Graphic extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = { maxWidth: 960, width: 960, height: 500 };
+  }
+  handleResize() {
+    let window_width = Math.min(this.state.maxWidth, this.refs.graphicContainer.offsetWidth)
+      this.setState({ width : window_width });
+  }
+  componentDidMount() {
+      this.handleResize(this);
+      window.addEventListener('resize', this.handleResize.bind(this));
+  }
+  componentWillUnmount() {
+      window.removeEventListener('resize', this.handleResize.bind(this));
+  }
   render() {
     var el = ReactFauxDOM.createElement('div')
-    //el.setAttribute('class', 'graphicContainer') 
     
-    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+    var margin = { top: 20, right: 20, bottom: 30, left: 80 },
+      width = this.state.width - margin.left - margin.right,
+      height = this.state.height - margin.top - margin.bottom;
 
-    var x = d3.scale.ordinal().rangeRoundBands([0,width], .1);
-    var y = d3.scale.linear().range([height,0]);
-    var xAxis = d3.svg.axis().scale(x).orient('bottom')
-    var yAxis = d3.svg.axis().scale(y).orient('left').ticks(10,'%');
+    var x = d3.scale.linear().range([0,width]);
+    var y = d3.scale.ordinal().rangeRoundBands([0,height], .1);
+
+    var xAxis = d3.svg.axis().scale(x).orient('bottom').ticks(10);
+    var yAxis = d3.svg.axis().scale(y).orient('left');
 
     var svg = d3.select(el).append('svg')
         .attr('width', width + margin.left + margin.right)
@@ -57,65 +80,51 @@ class Graphic extends React.Component {
 
     var data = this.props.data
 
-    x.domain(data.map(function(d) { return d[0]; }));
-    y.domain([0, d3.max(data, function(d) { return d[1]; })]);
+    y.domain(data.map(function(d) { return d[0]; }));
+    x.domain([0, d3.max(data, function(d) { return d[1]; })]);
 
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .call(xAxis)
+      .append("text")
+        .attr("x", this.state.width/2)
+        .attr("y", 21)
+        .attr("dy", ".71em")
+        .style("text-anchor", "middle")
+        .text("Word Count");
 
     svg.append("g")
         .attr("class", "y axis")
-        .call(yAxis)
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Frequency");
+        .call(yAxis);
 
     svg.selectAll(".bar")
         .data(data)
       .enter().append("rect")
         .attr("class", "bar")
-        .attr("x", function(d) { return x(d[0]); })
-        .attr("width", x.rangeBand())
-        .attr("y", function(d) { return y(d[1]); })
-        .attr("height", function(d) { return height - y(d[1]); });
+        .attr("y", function(d) { return y(d[0]); })
+        .attr("height", y.rangeBand())
+        .attr("x", function(d) { return 0; })
+        .attr("width", function(d) { return x(d[1]); });
 
-    function type(d) {
-      d.frequency = +d.frequency;
-      return d;
-    }
-
-    return el.toReact()
-  }
-};
-
-class Graphic2 extends React.Component {
-  render() {
-    console.log(this.props.data)
-    return (<div className='graphicContainer'>
-      This is the container
-      {this.props.data.map(item=><div>
-        <div>{item[0]}</div>
-        <div>{item[1]}</div>
-        </div>)}
-      </div>);
+    return (<div className= 'graphicContainer' ref='graphicContainer'>{el.toReact()}</div>)
   }
 };
 
 class Dropdown extends React.Component {
   render() {
-    return (<select className='dropdown'>
-        <option value="nytimes.com">New York Times</option>
-        <option value="theskint.com">The Skint</option>
-        <option value="garysguide.com">Gary's Guide</option>
+    return (<select className='dropdown' onChange={this.props.handleDropdownChange}>
+        <option value="http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml">New York Times Homepage</option>
+        <option value="http://www.wsj.com/xml/rss/3_7014.xml">WSJ Business News</option>
+        <option value="http://feeds.washingtonpost.com/rss/national">Washington Post National</option>
+        <option value="http://www.buzzfeed.com/index.xml">Buzz Feed Homepage</option>
+        <option value="http://theskint.com/feed">The Skint (NYC Events)</option>
+        <option value="http://nypost.com/feed">NY Post All Stories</option>
+        <option value="http://www.newyorker.com/feed/everything">New Yorker - All Stories</option>
+        <option value="http://www.theguardian.com/us/rss">The Guardian - US</option>
+        <option value="http://feeds.theguardian.com/theguardian/uk/rss">The Guardian - UK</option>
       </select>);
   }
 }
 
 render(<App/>, document.getElementById('app'));
-
-
